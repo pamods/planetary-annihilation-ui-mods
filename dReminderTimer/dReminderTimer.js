ecd007ece983672b8746a2c865018f9b7eb3c418
David Leaver <danzel@localhost.geek.nz>
2013-11-08 07:23:49 +1300

    Scrap having this be a replacement for dTimer, redo everything. Looks ok, not implemented yet.

--- dReminderTimer/dReminderTimer.js
+++ dReminderTimer/dReminderTimer.js
@@ -1,66 +1,65 @@
-window.reminderTimer = {
+var reminderDef = function (name, time, repeat, audio, visible, hasConfirm) {
+	this.name = ko.observable(name);
+	this.time = ko.observable(time);
+	this.repeat = repeat;
+	this.audio = audio;
+	this.visible = visible;
+	this.hasConfirm = hasConfirm;
+};
+
+model.reminderTimer = {
 	init: function() {
 	
-		//Create div, add to page, knockout bind it
-		this.element = $('<div id="in_game_timer" data-bind="text: currentTime">Sync</div>');
-		$('body').append(this.div);
-		ko.applyBindings(this, this.element[0]);
+		//Create div, add to page
+		this.manage = $(
+			'<div id="remindertimer_manage">' +
+				'<h1>Manage Reminders</h1>' +
+				'<table>' +
+					'<tr><th>Select Reminder</th><td><select><option>Reminder A</option></select></td></tr>' +
+					'<tr><th>Name</th><td><input /></td></tr>' +
+					'<tr><th>Time</th><td><input type="number" />:<input type="number" /> (mm:ss)</td></tr>' +
+					'<tr><td colspan="2">When this timer expires, do the following</td></tr>' +
+					'<tr><th>Repeat</th><td><input type="checkbox" /> Repeat</td></tr>' +
+					'<tr><th>Visible</th><td><input type="checkbox" /> Show a message</td></tr>' +
+					'<tr><th>Confirm</th><td><input type="checkbox" /> Ask for confirmation it has been acted</td></tr>' +
+					'<tr><th>Audio</th><td><input type="checkbox" /> Play an audio queue</td></tr>' +
+					'<tr><th></th><td><button>Save</button> <button>Delete</button></td></tr>' +
+				'</table>' +
+			'</div>');
 
-		//On click we try sync
-		this.element.click($.proxy(this.syncFunction, this));
-	
-		//Hack in to the method that is called when [Start Annihilation] is clicked
-		var oldAck = model.ackMessage;
-		model.ackMessage = $.proxy(function() {
-			oldAck();
-			this.beginSync();
-		}, this);
+		this.timersEl = $(
+			'<div id="remindertimers">' +
+				'<div class="timer">(mm:ss) Scout More</div>' +
+				'<div class="timer">(mm:ss) Check Nuke</div>' +
+				'<div class="timer">(mm:ss) Hide Comm</div>' +
+
+				'<div class="managebutton">Manage</div>' +
+
+				'<div class="quicktimer">' +
+					'Quick <input type="text" /><br/>' +
+					'<button>1m</button>' +
+					'<button>2m</button>' +
+					'<button>3m</button>' +
+					'<button>4m</button>' +
+					'<button>5m</button>' +
+				'</div>' +
+			'</div>');
+
+		//$('body').append(this.manage);
+		$('.div_player_list_panel').append(this.timersEl);
+
+		//Try really hard not to retain focus so we don't break keyboard shortcuts
+		$('select', this.container).change(this.blurAll);
+		$(':checkbox', this.container).click(this.blurAll);
+		$(document).keydown($.proxy(function (e) {
+			if (e.keyCode == 27) {
+				this.blurAll();
+			}
+		}, this));
 	},
 
-	currentTime: ko.observable('Sync'),
-	
-	updateTime: function() {
-		var newTime = this.timeOffset + (new Date().getTime() / 1000);
-		var time = parseInt(newTime);
-		
-		var seconds = time % 60;
-		var minutes = parseInt(time / 60) % 60;
-		var hours = parseInt(time / 60 / 60);
-
-		var str = (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
-		if (hours > 0) {
-			str = hours + ":" + str;
-		}
-		this.currentTime(str);
+	blurAll: function () {
+		$(document.activeElement).blur();
 	},
-
-	beginSync: function() {
-		clearInterval(this.syncInterval);
-		clearInterval(this.trySyncInterval);
-
-		this.syncStartTime = model.currentTimeInSeconds();
-		api.time.control();
-		//if you set and unset immediately then current time doesn't work, so use a wait for time to change thingy
-		this.attemptSyncInterval = setInterval($.proxy(this.attemptSync, this), 100);
-	},
-
-	attemptSync: function () {
-		var newTime = model.currentTimeInSeconds();
-		if (newTime == this.syncStartTime) {
-			//time hasn't changed, keep waiting...
-			//console.log('waiting');
-			return;
-		}
-
-		clearInterval(this.attemptSyncInterval);
-		api.time.resume();
-
-		this.timeOffset = newTime - (new Date().getTime() / 1000);
-
-		//console.log('got it!' + newTime + ' ' + timeOffset);
-
-		this.updateTime();
-		this.syncInterval = setInterval($.proxy(this.updateTime, this), 1000);
-	}
 };
-window.reminderTimer.init();
+model.reminderTimer.init();
